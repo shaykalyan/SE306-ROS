2af84173f6156f43006a1899c890930f820cf7c7
nweatherburn <n.weatherburn@gmail.com>
2014-08-08 11:49:14 +1200

    Changed robot so that he turns the shortest direction

--- elderly_care_simulation/src/Navigater.cpp
+++ elderly_care_simulation/src/Navigater.cpp
@@ -25,7 +25,7 @@ ros::Subscriber navigationSubscriber;
 geometry_msgs::Pose currentLocation;
 
 // Desired location of the robot
-geometry_msgs::Pose desiredLocation;
+geometry_msgs::Point desiredLocation;
 
 // Current Velocity
 geometry_msgs::Twist currentVelocity;
@@ -54,17 +54,13 @@ void stageOdometryCallback(const nav_msgs::Odometry msg)
     
     double roll, pitch, yaw;
     tf::Matrix3x3(tf::Quaternion(x, y, z, w)).getRPY(roll, pitch, yaw);
-
     currentAngle = yaw;
 }
 
-void updateDesiredLocationCallback(const geometry_msgs::Pose location)
+void updateDesiredLocationCallback(const geometry_msgs::Point location)
 {   
     // Update desired location
     desiredLocation = location;
-    
-    // Log current location details
-    logLocation("Desired Location is now:", desiredLocation);
 }
 
 void normalize(geometry_msgs::Vector3 &vector)
@@ -84,26 +80,53 @@ bool doubleEquals(double a, double b, double difference)
     return std::abs(a - b) < difference;
 }
 
+double normalizeAngle(double angle)
+{
+    while (angle < 0) {
+        angle += 2 * M_PI;
+    }
+    while (angle > 2 * M_PI) {
+        angle -= 2 * M_PI;
+    }
+    return angle;
+}
+
+bool turnAnticlockwise(double currentAngle, double desiredAngle)
+{   
+    if (currentAngle < 0) {
+        currentAngle = 2 * M_PI + currentAngle;
+    }
+    if (desiredAngle < 0) {
+        desiredAngle = 2 * M_PI + desiredAngle;
+    }
+    desiredAngle = normalizeAngle(desiredAngle - currentAngle);
+    return desiredAngle < M_PI;
+    
+}
+
 void updateCurrentVelocity()
 {
     // Find the correct angle
     geometry_msgs::Point directionVector; // Vector from currentLocation to desiredLocation
 
-    directionVector.x = desiredLocation.position.x - currentLocation.position.x;
-    directionVector.y = desiredLocation.position.y - currentLocation.position.y;
-    directionVector.z = desiredLocation.position.z - currentLocation.position.z;
+    directionVector.x = desiredLocation.x - currentLocation.position.x;
+    directionVector.y = desiredLocation.y - currentLocation.position.y;
+    directionVector.z = desiredLocation.z - currentLocation.position.z;
     
     // Thank god we're only doing 2D stuff
     double desiredAngle = atan2(directionVector.y, directionVector.x);
+
     ROS_INFO("DESIRED ANGLE: %f", desiredAngle);
     ROS_INFO("CURRENT ANGLE: %f", currentAngle);
+
     
-    if (! doubleEquals(currentAngle, desiredAngle, 0.15)) {
+    if (! doubleEquals(currentAngle, desiredAngle, 0.2)) {
         // Turn towards angle
         currentVelocity.linear.x = 0;
         currentVelocity.linear.y = 0;
         currentVelocity.linear.z = 0;
-        if (directionVector.y < 0) {
+        
+        if (turnAnticlockwise(currentAngle, desiredAngle)) {
             // Turn anti clockwise
             currentVelocity.angular.z = 1;
         } else {
@@ -125,9 +148,9 @@ bool atDesiredLocation()
     //logLocation("Current location: ", currentLocation);
     //logLocation("Desired location: ", desiredLocation);
     //return false;    
-    return doubleEquals(currentLocation.position.x, desiredLocation.position.x, toleratedDifference) &&
-           doubleEquals(currentLocation.position.y, desiredLocation.position.y, toleratedDifference) &&
-           doubleEquals(currentLocation.position.z, desiredLocation.position.z, toleratedDifference);
+    return doubleEquals(currentLocation.position.x, desiredLocation.x, toleratedDifference) &&
+           doubleEquals(currentLocation.position.y, desiredLocation.y, toleratedDifference) &&
+           doubleEquals(currentLocation.position.z, desiredLocation.z, toleratedDifference);
       
 }
 
