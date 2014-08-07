3e32d01636b37134984c250561ebc11c0dc655df
Shay Kalyan <akshaykalyan@outlook.com>
2014-08-07 22:33:11 +1200

    Assistant now spins for 4 seconds (one full rotation) on event trigger. @bert-huang

--- elderly_care_simulation/src/Assistant.cpp
+++ elderly_care_simulation/src/Assistant.cpp
@@ -21,6 +21,7 @@ double px;
 double py;
 double theta;
 
+ros::Publisher RobotNode_stage_pub;
 ros::Publisher EventTrigger_pub;
 ros::Subscriber EventTrigger_sub;
 
@@ -29,8 +30,6 @@ void StageOdom_callback(nav_msgs::Odometry msg)
 	//This is the call back function to process odometry messages coming from Stage. 	
 	px = WORLD_POS_X + msg.pose.pose.position.x;
 	py = WORLD_POS_Y + msg.pose.pose.position.y;
-	// ROS_INFO("Current x position is: %f", px);
-	// ROS_INFO("Current y position is: %f", py);
 }
 
 
@@ -42,12 +41,14 @@ void StageLaser_callback(sensor_msgs::LaserScan msg)
 }
 
 void EventTrigger_reply() {
+	// create response message
 	elderly_care_simulation::EventTrigger msg;
 	msg.msg_type = EVENT_TRIGGER_MSG_TYPE_RESPONSE;
 	msg.event_type = EVENT_TRIGGER_EVENT_TYPE_ASSISTANT;
 	msg.result = EVENT_TRIGGER_RESULT_SUCCESS;
 
 	EventTrigger_pub.publish(msg);
+	ROS_INFO("Assistant Reply Message Sent");
 }
 
 void EventTrigger_callback(elderly_care_simulation::EventTrigger msg)
@@ -57,9 +58,21 @@ void EventTrigger_callback(elderly_care_simulation::EventTrigger msg)
 			ROS_INFO("Assistant Message Recieved");
 
 			// carry out activity
-			sleep(5); 
+			// update angular z and inform stage
+			geometry_msgs::Twist RobotNode_cmdvel;
+			RobotNode_cmdvel.linear.x = linear_x;
+			RobotNode_cmdvel.angular.z = 2.0;
+			RobotNode_stage_pub.publish(RobotNode_cmdvel);
 
-			// reply done function
+			// stall for 5 seconds to allow robot to spin
+			sleep(4);
+
+			// reset angular z and update stage
+			RobotNode_cmdvel.linear.x = linear_x;
+			RobotNode_cmdvel.angular.z = 0.0;
+			RobotNode_stage_pub.publish(RobotNode_cmdvel);
+
+			// trigger successful reply
 			EventTrigger_reply();
 		}
 	}
@@ -68,7 +81,7 @@ void EventTrigger_callback(elderly_care_simulation::EventTrigger msg)
 int main(int argc, char **argv)
 {
 
- //initialize robot parameters
+ 	//initialize robot parameters
 	//Initial pose. This is same as the pose that you used in the world file to set	the robot pose.
 	theta = M_PI/2.0;
 	px = WORLD_POS_X;
@@ -78,46 +91,45 @@ int main(int argc, char **argv)
 	linear_x = 0.0;
 	angular_z = 0.0;
 	
-//You must call ros::init() first of all. ros::init() function needs to see argc and argv. The third argument is the name of the node
-ros::init(argc, argv, "Assistant");
+	//You must call ros::init() first of all. ros::init() function needs to see argc and argv. The third argument is the name of the node
+	ros::init(argc, argv, "Assistant");
 
-//NodeHandle is the main access point to communicate with ros.
-ros::NodeHandle n;
+	//NodeHandle is the main access point to communicate with ros.
+	ros::NodeHandle n;
 
-//advertise() function will tell ROS that you want to publish on a given topic_
-//to stage
-ros::Publisher RobotNode_stage_pub = n.advertise<geometry_msgs::Twist>("robot_3/cmd_vel",1000);
-EventTrigger_pub = n.advertise<elderly_care_simulation::EventTrigger>("event_trigger",1000, true);
+	//advertise() function will tell ROS that you want to publish on a given topic_
+	//to stage
+	RobotNode_stage_pub = n.advertise<geometry_msgs::Twist>("robot_3/cmd_vel",1000);
+	EventTrigger_pub = n.advertise<elderly_care_simulation::EventTrigger>("event_trigger",1000, true);
 
-//subscribe to listen to messages coming from stage
-ros::Subscriber StageOdo_sub = n.subscribe<nav_msgs::Odometry>("robot_3/odom",1000, StageOdom_callback);
-ros::Subscriber StageLaser_sub = n.subscribe<sensor_msgs::LaserScan>("robot_3/base_scan",1000,StageLaser_callback);
-EventTrigger_sub = n.subscribe<elderly_care_simulation::EventTrigger>("event_trigger",1000, EventTrigger_callback);
+	//subscribe to listen to messages coming from stage
+	ros::Subscriber StageOdo_sub = n.subscribe<nav_msgs::Odometry>("robot_3/odom",1000, StageOdom_callback);
+	ros::Subscriber StageLaser_sub = n.subscribe<sensor_msgs::LaserScan>("robot_3/base_scan",1000,StageLaser_callback);
+	EventTrigger_sub = n.subscribe<elderly_care_simulation::EventTrigger>("event_trigger",1000, EventTrigger_callback);
 
-ros::Rate loop_rate(10);
+	ros::Rate loop_rate(10);
 
-//a count of howmany messages we have sent
-int count = 0;
+	//a count of howmany messages we have sent
+	int count = 0;
 
-////messages
-//velocity of this RobotNode
-geometry_msgs::Twist RobotNode_cmdvel;
+	////messages
+	//velocity of this RobotNode
+	geometry_msgs::Twist RobotNode_cmdvel;
 
-while (ros::ok())
-{
-	//messages to stage
-	RobotNode_cmdvel.linear.x = linear_x;
-	RobotNode_cmdvel.angular.z = angular_z;
-        
-	//publish the message
-	RobotNode_stage_pub.publish(RobotNode_cmdvel);
-	
-	ros::spinOnce();
+	while (ros::ok())
+	{
+		//messages to stage
+		RobotNode_cmdvel.linear.x = linear_x;
+		RobotNode_cmdvel.angular.z = angular_z;
+	        
+		//publish the message
+		RobotNode_stage_pub.publish(RobotNode_cmdvel);
+		
+		ros::spinOnce();
 
-	loop_rate.sleep();
-	++count;
-}
+		loop_rate.sleep();
+		++count;
+	}
 
-return 0;
-
+	return 0;
 }
