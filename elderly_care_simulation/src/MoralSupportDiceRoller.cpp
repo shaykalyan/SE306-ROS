b9243d1ec5317f1df05a745de65851aadd3edd91
Matthew Chiam <mchiam1991@gmail.com>
2014-08-14 22:53:22 +1200

    Refactored Dice Rollers into classes so they can be easily tested.

--- elderly_care_simulation/src/MoralSupportDiceRoller.cpp
+++ elderly_care_simulation/src/MoralSupportDiceRoller.cpp
@@ -5,11 +5,38 @@
 #include <cstdlib>
 #include "math.h"
 
+#include "MoralSupportDiceRoller.h"
 #include "DiceRollerTypeConstants.h"
 #include "elderly_care_simulation/DiceRollTrigger.h"
 
-// Number of sides this dice has
-const int DICE_SIDES = 300;
+MoralSupportDiceRoller::MoralSupportDiceRoller() {
+    threshold = DICE_SIDES;
+}
+
+MoralSupportDiceRoller::~MoralSupportDiceRoller() {
+}
+
+/**
+ * Rolls the dice. If the roll meets the threshold,
+ * the threshold is reset to the worst probability
+ * and true is returned, otherwise probability is 
+ * increased and false is returned.
+ */
+bool MoralSupportDiceRoller::roll() {
+
+    int rolled = rand() % DICE_SIDES + 1;
+    ROS_INFO("Rolled: %d. Needed: %d", rolled, threshold);
+    
+    if (rolled >= threshold) {
+        // Reset threshold
+        threshold = DICE_SIDES;
+        return true;
+    } 
+    else {
+        threshold--;
+        return false;
+    }   
+}
 
 // Signatures
 ros::Publisher diceTriggerPub;
@@ -25,35 +52,27 @@ int main(int argc, char **argv) {
     // Declare publishers
     diceTriggerPub = nodeHandle.advertise<elderly_care_simulation::DiceRollTrigger>("dice_roll_trigger", 1000, true);
 
+    // Create diceroller
+    MoralSupportDiceRoller roller = MoralSupportDiceRoller();
+
     int tick = 1;
-    int threshold = DICE_SIDES;
 
     while (ros::ok()) {
         
-        // Roll dice every 10 ticks
         if (tick % 10 == 0) {
 
-            int rolled = rand() % DICE_SIDES + 1;
-            ROS_INFO("Rolled: %d. Needed: %d", rolled, threshold);
-            
-            if (rolled >= threshold) {
-
+            // Roll dice
+            bool result = roller.roll();
+            if (result) {
                 ROS_INFO("YOUR MORALE NEEDS REPLENISHING.");
                 diceRollTrigger.type = MORAL_SUPPORT;
                 diceTriggerPub.publish(diceRollTrigger);
-
-                // Reset threshold
-                threshold = DICE_SIDES;
             } 
-            else {
-
-                threshold--;
-            }
-            tick = 1;    
+            tick = 1;
         }
         else {
             tick++;
-        }        
+        }  
         
 	    ros::spinOnce();
 	    loop_rate.sleep();
