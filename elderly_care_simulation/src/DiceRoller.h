80383bbf5507b632ce432da6838de5fe170d92b4
Matthew Chiam <mchiam1991@gmail.com>
2014-08-16 22:28:39 +1200

    Created parent class for Dice Rollers and had them inherit off it. Fixes #25.

---
+++ elderly_care_simulation/src/DiceRoller.h
@@ -0,0 +1,31 @@
+#ifndef DICE_ROLLER_H
+#define DICE_ROLLER_H
+
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+
+#include <sstream>
+#include <cstdlib>
+#include "math.h"
+
+#include "DiceRollerTypeConstants.h"
+#include "elderly_care_simulation/DiceRollTrigger.h"
+
+class DiceRoller {
+
+    public:
+
+        DiceRoller(int sides);
+        ~DiceRoller();
+
+        bool roll();
+
+    private:
+
+        int DICE_SIDES; // not const as it needs to be assigned in constructor (work-around)
+        int threshold;
+};
+
+#endif
+
+
