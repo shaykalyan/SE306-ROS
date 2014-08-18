233c0dfe28847ec9176690004054b32dfa661df8
Bert Huang <ihua164@aucklanduni.ac.nz>
2014-08-18 23:04:40 +1200

    Fixed priority queue unstable for nodes with same priority. <@xShay | @bert-huang>

--- elderly_care_simulation/src/EventNode.h
+++ elderly_care_simulation/src/EventNode.h
@@ -4,19 +4,30 @@ class EventNode {
 public:
 	EventNode(int priority, elderly_care_simulation::EventTrigger msg);
 	int getPriority() const;
+	int getWeight() const;
 	elderly_care_simulation::EventTrigger getEventTriggerMessage() const;
 private:
+	static int _count;
+	int _weight;
 	int _priority;
 	elderly_care_simulation::EventTrigger _msg;
 };
 
+int EventNode::_count = 0;
+
 EventNode::EventNode(int priority, elderly_care_simulation::EventTrigger msg) {
 	_priority = priority;
 	_msg = msg;
+	_weight = EventNode::_count;
+	EventNode::_count++;
 }
 
 int EventNode::getPriority() const {
 	return _priority;
+}
+
+int EventNode::getWeight() const {
+	return _weight;
 }
 
 elderly_care_simulation::EventTrigger EventNode::getEventTriggerMessage() const {
@@ -24,5 +35,8 @@ elderly_care_simulation::EventTrigger EventNode::getEventTriggerMessage() const 
 }
 
 bool operator<(EventNode lhs, EventNode rhs) {
+	if (lhs.getPriority() == rhs.getPriority()){
+		return lhs.getWeight() > rhs.getWeight();
+	}
 	return lhs.getPriority() > rhs.getPriority();
 }
