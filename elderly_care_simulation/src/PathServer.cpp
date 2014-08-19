9262a1b1bb38dbaaf79939d42feb72d97d3dbacc
nweatherburn <n.weatherburn@gmail.com>
2014-08-20 00:39:31 +1200

    #33 Added helper topic to check whether there is a wall at a specific point

--- elderly_care_simulation/src/PathServer.cpp
+++ elderly_care_simulation/src/PathServer.cpp
@@ -9,10 +9,33 @@
 // The map of this 
 nav_msgs::OccupancyGrid occupancyGrid;
 
+// Width and Height of the map in pixels
+int width;
+int height;
+
+// Bottom Left position
+float bottom;
+float top;
+float left;
+float right;
+
+// The number of pixels per meter in stage
+float resolution;
+
+
+
+/**
+ * Helper function to return the int value 
+ * at the x, y position in occupancyGrid.data.
+ */
+int occupancyAtPoint(int x, int y)
+{
+    return occupancyGrid.data[y * width + x];
+}
+
 /**
  * Returns a vector of geometry_msgs::Point objects that indicate the best path to take
  * from point 'from' to point 'to'.
- * 
  */
 bool findPath(elderly_care_simulation::FindPath::Request  &req,
                elderly_care_simulation::FindPath::Response &res)
@@ -20,6 +43,65 @@ bool findPath(elderly_care_simulation::FindPath::Request  &req,
     // Find path
     ROS_WARN("Finding the path not implemented yet");
     return true;
+}
+
+/**
+ * TODO: Remove
+ */ 
+void is_vacant(geometry_msgs::Point point)
+{
+
+    if (point.x < left || point.x > right ||
+        point.y < bottom || point.y > top) {
+        ROS_INFO("OUT OF BOUNDS at position %f, %f", point.x, point.y);
+        return;
+    }
+
+    int x = round((resolution / (right - left)) * (point.x - left)   * width  / resolution);
+    int y = round((resolution / (top - bottom)) * (point.y - bottom) * height / resolution);
+
+    ROS_INFO("X: %d", x);
+    ROS_INFO("Y: %d", y);
+
+    if (occupancyAtPoint(x, y) > 0) {
+        ROS_INFO("Wall at position %f, %f", point.x, point.y);
+    } else {
+        ROS_INFO("Empty at position %f, %f", point.x, point.y);
+    }
+}
+
+void printFirst()
+{
+    for (int y = 0; y < height; ++y) {
+        for (int x = 0; x < width; ++x) {
+             if (occupancyGrid.data[y * width + x] > 0) {
+                ROS_INFO("X: %d", x);
+                ROS_INFO("Y: %d", y);
+                ROS_INFO("Value: %d", occupancyGrid.data[y * width + x]);
+                return;
+             } 
+        }
+    }
+}
+
+void initializeHelperVariables()
+{
+    width      = occupancyGrid.info.width;
+    height     = occupancyGrid.info.height;
+    bottom     = occupancyGrid.info.origin.position.y;
+    left       = occupancyGrid.info.origin.position.x;
+    resolution = occupancyGrid.info.resolution;
+
+    top        = bottom + (height / resolution);
+    right      = left + (width / resolution);
+
+    ROS_INFO("Width: %d", width);
+    ROS_INFO("Height: %d", height);
+    ROS_INFO("Bottom: %f", bottom);
+    ROS_INFO("Top: %f", top);
+    ROS_INFO("Left: %f", left);
+    ROS_INFO("Right: %f", right);
+    ROS_INFO("Resolution: %f", resolution);
 }
 
 int main(int argc, char **argv)
@@ -32,11 +114,15 @@ int main(int argc, char **argv)
     // Map server that offers a get_map service
     ros::ServiceClient mapServer = pathNodeHandle.serviceClient<nav_msgs::GetMap>("static_map");
 
+    // TODO: Remove
+    ros::Subscriber pathToRobotSub = pathNodeHandle.subscribe<geometry_msgs::Point>("is_vacant", 1000, is_vacant);
+
     nav_msgs::GetMap getMapService;
     if (mapServer.call(getMapService)) {
         occupancyGrid = getMapService.response.map;
-        ROS_INFO("Width: %d\nHeight: %d", occupancyGrid.info.width, occupancyGrid.info.height);
-        ROS_INFO("Origin: %f, %f", occupancyGrid.info.origin.position.x, occupancyGrid.info.origin.position.y);
+        initializeHelperVariables();
+
+        printFirst(); // TODO: Remove
     } else {
         ROS_WARN("Could not retrieve map from mapserver");
     }
