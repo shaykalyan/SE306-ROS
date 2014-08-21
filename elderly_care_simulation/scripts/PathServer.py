198e848f0a5d5ba90da8fcf51a1c3da20d33d8e5
nweatherburn <n.weatherburn@gmail.com>
2014-08-21 15:43:38 +1200

    Fixed indentation errors. [#33]

--- elderly_care_simulation/scripts/PathServer.py
+++ elderly_care_simulation/scripts/PathServer.py
@@ -101,46 +101,46 @@ def find_path_server():
     
 
 def check_vacancy_at_cell(house_map, cell):
-	"""
-	Return True if the given cell is vacant.
+    """
+    Return True if the given cell is vacant.
 
     Vacancy is defined as a '0' in the house map at the given coordinates.
     (i.e. there is no wall at that location)
-	"""
-	x = cell[0]
-	y = cell[1]
-	
-	if not 0 <= x < MAP_WIDTH:
-		return False
-	
-	if not 0 <= y < MAP_HEIGHT:
-		return False
-		
-	return house_map[y][x] == '0'
+    """
+    x = cell[0]
+    y = cell[1]
+    
+    if not 0 <= x < MAP_WIDTH:
+        return False
+    
+    if not 0 <= y < MAP_HEIGHT:
+        return False
+        
+    return house_map[y][x] == '0'
     
     
 def get_vacant_neighbours(house_map, cell):
-	"""
+    """
     Get a list of coordinates that are vacant around the given cell coordinates.
 
     Only neighbours to the top, bottom, left and right are considered.
-	
+    
     'house_map': a list of lists. Each list is a row of 0s and 1s (vacant cells and walls respectively)
-	'cell': a tuple: (x, y) with the origin at the bottom left
+    'cell': a tuple: (x, y) with the origin at the bottom left
 
     return: a list of up to four (x, y) tuples, each representing a neighbour of 'cell' that is vacant
-	"""
-	x = cell[0]
-	y = cell[1]
-	
-	neighbours = [(x, y + 1), (x + 1, y), (x, y - 1), (x - 1, y)]
-	vacant_neighbours = []
-	
-	for neighbour in neighbours:
-		if check_vacancy_at_cell(house_map, neighbour):
-			vacant_neighbours.append(neighbour)
-			
-	return vacant_neighbours
+    """
+    x = cell[0]
+    y = cell[1]
+    
+    neighbours = [(x, y + 1), (x + 1, y), (x, y - 1), (x - 1, y)]
+    vacant_neighbours = []
+    
+    for neighbour in neighbours:
+        if check_vacancy_at_cell(house_map, neighbour):
+            vacant_neighbours.append(neighbour)
+            
+    return vacant_neighbours
     
     
 def generate_graph(filename):
@@ -157,22 +157,22 @@ def generate_graph(filename):
 
     'filename': the name of a pgm file of the map 
     """
-	house_map = []
-	
-	with open(filename, 'r') as f:
-		lines = f.read().splitlines()
-	
-	# Remove pgm file information
-	del lines[:3]
-	
-	for line in reversed(lines):
-		house_map.append(line.split())
-	
-	for y, row in enumerate(house_map):
-		for x, cell in enumerate(row):
-			cell = (x, y)
-			graph[cell] = get_vacant_neighbours(house_map, cell)
-		
+    house_map = []
+    
+    with open(filename, 'r') as f:
+        lines = f.read().splitlines()
+    
+    # Remove pgm file information
+    del lines[:3]
+    
+    for line in reversed(lines):
+        house_map.append(line.split())
+    
+    for y, row in enumerate(house_map):
+        for x, cell in enumerate(row):
+            cell = (x, y)
+            graph[cell] = get_vacant_neighbours(house_map, cell)
+        
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
