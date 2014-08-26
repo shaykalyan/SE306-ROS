dd9ad1909658205947b189b71a4b245bc14b1532
nweatherburn <n.weatherburn@gmail.com>
2014-08-26 22:19:39 +1200

    Removed the need for callback functions. Callbacks are now passed to the class methods directly.

--- elderly_care_simulation/src/FeedingRobot.cpp
+++ elderly_care_simulation/src/FeedingRobot.cpp
@@ -11,20 +11,6 @@
 #include "PerformTaskConstants.h" 
 #include "Robot.h"
 
-EscortRobot feeder;
-
-void eventTriggeredCallback(elderly_care_simulation::EventTrigger msg) {
-    feeder.eventTriggered(msg);
-}
-
-void stageCallBack(nav_msgs::Odometry msg) {
-    feeder.stage0domCallback(msg);
-}
-
-void residentStageCallback(nav_msgs::Odometry msg) {
-    feeder.residentLocationCallback(msg);
-}
-
 int main(int argc, char **argv) {
 
     geometry_msgs::Point base;
@@ -35,7 +21,7 @@ int main(int argc, char **argv) {
     table.x = ADJACENT_TABLE_X;
     table.y = ADJACENT_TABLE_Y;
 
-    feeder = EscortRobot(EVENT_TRIGGER_EVENT_TYPE_EAT, base, table);
+    EscortRobot feeder(EVENT_TRIGGER_EVENT_TYPE_EAT, base, table);
 
     const std::string rid = "robot_10";
 
@@ -48,9 +34,10 @@ int main(int argc, char **argv) {
     feeder.robotNodeStagePub = chefNodeHandle.advertise<geometry_msgs::Twist>(rid + "/cmd_vel", 1000);
     feeder.eventTriggerPub = chefNodeHandle.advertise<elderly_care_simulation::EventTrigger>("event_trigger", 1000, true);
 
-    feeder.stageOdoSub = chefNodeHandle.subscribe<nav_msgs::Odometry>(rid + "/base_pose_ground_truth", 1000, stageCallBack);
-    feeder.eventTriggerSub = chefNodeHandle.subscribe<elderly_care_simulation::EventTrigger>("event_trigger", 1000, eventTriggeredCallback);
-    feeder.residentLocationSub = chefNodeHandle.subscribe<nav_msgs::Odometry>("robot_0/base_pose_ground_truth", 1000, residentStageCallback);
+    // Necessary subscribers
+    feeder.stageOdoSub = chefNodeHandle.subscribe<nav_msgs::Odometry>(rid + "/base_pose_ground_truth", 1000, &Robot::stage0domCallback, dynamic_cast<Robot*>( &feeder ));
+    feeder.eventTriggerSub = chefNodeHandle.subscribe<elderly_care_simulation::EventTrigger>("event_trigger", 1000, &EscortRobot::eventTriggered, &feeder);
+    feeder.residentLocationSub = chefNodeHandle.subscribe<nav_msgs::Odometry>("robot_0/base_pose_ground_truth", 1000, &EscortRobot::residentLocationCallback, &feeder);
 
     // Service used to find paths
     feeder.pathFinderService = chefNodeHandle.serviceClient<elderly_care_simulation::FindPath>("find_path");
