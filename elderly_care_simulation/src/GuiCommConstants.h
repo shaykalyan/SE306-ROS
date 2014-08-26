2c72ba6b23f7ba0f452088581333c3883b567421
Shay Kalyan <akshaykalyan@outlook.com>
2014-08-26 17:04:56 +1200

    Added ROS message type and constants header file for GuiComms [#106]

---
+++ elderly_care_simulation/src/GuiCommConstants.h
@@ -0,0 +1,16 @@
+#ifndef GUI_COMM_MSG_INCLUDE
+#define GUI_COMM_MSG_INCLUDE 1
+
+// ========================================
+// =           MESSAGE TYPE               =
+// ========================================
+const int GUI_COMM_MSG_TYPE_REQUEST = 0;
+const int GUI_COMM_MSG_TYPE_INFO    = 1;
+
+// ========================================
+// =               ACTION                 =
+// ========================================
+const int GUI_COMM_ACTION_POPULATE  = 0;
+const int GUI_COMM_ACTION_CLEAR     = 1;
+
+#endif
